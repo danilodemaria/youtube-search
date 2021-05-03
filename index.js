d537ce822524c918e4bcf6a969e56e8649e3b069
Danilo de Maria <daniloctg@msn.com>
2021-05-03 13:35:19 -0300

    fix fix

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 const express = require('express');
+const cors = require('cors');
 const { spawn } = require('child_process');
 const app = express();
 const port = 4444;
+
+app.use(express.json());
+app.use(
+  cors({
+    origin: ['https://danilodemaria.com.br'],
+  })
+);
 
 app.get('/:title', (req, res) => {
   console.log('to aqui', title);
@@ -17,7 +25,7 @@ app.get('/:title', (req, res) => {
     res.send(dataToSend);
   });
 });
-app.listen(port, () =>
+app.listen(port, '0.0.0.0', () =>
   console.log(`Listening on port
    ${port}!`)
 );
