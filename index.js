6e5bad2c27bba188e73ace741a7bed665ba693f0
Danilo de Maria <daniloctg@msn.com>
2021-05-03 13:12:41 -0300

    fix port

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const { spawn } = require('child_process');
 const app = express();
-const port = 3000;
+const port = 4444;
 
 app.get('/:title', (req, res) => {
   const { title } = req.params;
