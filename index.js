799a133b15c1fb30c3a091b621b87bb6834710a8
Danilo de Maria <daniloctg@msn.com>
2021-05-03 13:32:18 -0300

    fix

--- index.js
+++ index.js
@@ -4,13 +4,16 @@ const app = express();
 const port = 4444;
 
 app.get('/:title', (req, res) => {
+  console.log('to aqui', title);
   const { title } = req.params;
   let dataToSend;
   const python = spawn('python', ['you2.py', title]);
   python.stdout.on('data', function (data) {
+    console.log('dentro');
     dataToSend = data.toString();
   });
   python.on('close', () => {
+    console.log('dentro do close agora');
     res.send(dataToSend);
   });
 });
