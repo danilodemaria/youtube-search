4769994828a7bbd13d30e4f2cb62ec2fe45ceb8d
Danilo de Maria <daniloctg@msn.com>
2021-05-10 16:58:52 -0300

    fix por

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.use(express.json());
 app.use(
   cors({
     credentials: true,
-    origin: ['http://localhost:4444'],
+    origin: ['http://localhost:4444', 'http://10.1.1.88:3000'],
   })
 );
 
